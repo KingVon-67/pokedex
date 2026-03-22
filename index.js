a1e95c7b7748a074827a752eb1ef8c6b5cd08076
KingVon-67 <isiah.mineur@cesjb.be>
2026-03-22 20:55:22 +0100

    troisieme requete : filtrer les pokemons par type

--- index.js
+++ index.js
@@ -79,4 +79,22 @@ app.get('/api/pokemon/:id',(req,res)=>{
 })
 
 
+// 3
+// chercher un pokemon par type
+app.get('/api/type/:type' ,(req,res)=>{
+  var TYPE = req.params.type.toLowerCase()
+  
+  var saltype = pokedex.filter(p => p.type == TYPE)
+  if (saltype.length == 0){
+    res.send("ERREUR 404")
+  }
+  else{
+      res.send(saltype)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 
